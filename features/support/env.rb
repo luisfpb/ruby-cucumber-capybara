4ddc792f51a04a74fdb9ed59d43b5e1691797a69
Luis Benassi <luis.benassi@walmart.com>
2016-08-25 21:57:47 -0300

    env - requires for capybara and environment setup

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,10 @@
 require 'rspec'
+require 'pry'
+require 'site_prism'
+require 'capybara/cucumber'
 
+Pry.config.color = true
+
+#Environment config
+ENV['ENVIRONMENT'] = 'prod' unless ENV.has_key? 'ENVIRONMENT'
+$environment = (YAML.load_file('./config/environment.yml'))[ENV['ENVIRONMENT']]
